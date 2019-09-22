f3ca3dedff3baf75ac9ac06936e5f167f2d13f10
alanmvargas <alanvargas@devgama.com>
2019-09-22 15:36:01 -0500

    //functions // Video Length: 9:45 // Video 9/35

---
+++ vstudio-js/functiondeclarations.js
@@ -0,0 +1,15 @@
+function sayHello(name)
+{
+    console.log("Hello Brotha " + name);
+}
+
+sayHello("Alan");
+
+
+function tax(amount)
+{
+    let result = amount * 0.16;
+    return result;
+}
+
+console.log(tax(100));
