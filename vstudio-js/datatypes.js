7638ef570c067999264c3202a7b47b7a56627791
alanmvargas <alanvargas@devgama.com>
2019-09-22 13:12:27 -0500

    fourth video

---
+++ vstudio-js/datatypes.js
@@ -0,0 +1,14 @@
+//Datatypes
+//Video length 5:42
+let number = 123;
+let word = 'Alan Vargas';
+let float = 12.343;
+let bool = true;
+let bool2 = false;
+let double = 124932432432.3324;
+
+console.log(typeof number);
+console.log(typeof word);
+console.log(typeof float);
+console.log(typeof bool);
+console.log(typeof double);
