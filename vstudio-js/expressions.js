d963bb6e04ffd5643daf4287702f449e3457084b
alanmvargas <alanvargas@devgama.com>
2019-09-22 14:15:02 -0500

    Expressions & Operators 7/35

---
+++ vstudio-js/expressions.js
@@ -0,0 +1,26 @@
+//Expressions
+// Video Length: 17:17
+// Video 7/35
+
+let a = 1;
+let b = 2;
+let c = a+b; // in this line, there are 3 expressions.
+// 1- let c, c is bien declared
+// 2- we are doing an operation of a and b
+// 3- the result is being assigned to C
+
+// Operators
+// assigment =
+// Arithmetic + - / * %
+// increment and decrement ++ --
+// Evaluation == = > < <> => <= ! ? 
+// String " "
+// Precedence
+var b = 1+2*3; // * goes first and then it adds. 
+// it will do as the following 2*3 = 6 + 1 = 7
+
+//function invocation operators
+//logical operators && ||
+//Member Accessor operator, it is the (PERIOD . )  that links functions with sub functions.
+//Code block operator { }
+// array element access operators []
