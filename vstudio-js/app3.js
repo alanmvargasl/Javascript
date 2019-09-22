1193f73d0cc6d848aa114699f2f2dac2d733a764
alanmvargas <alanvargas@devgama.com>
2019-09-22 12:46:19 -0500

    Third Video

---
+++ vstudio-js/app3.js
@@ -0,0 +1,10 @@
+// Video Length 14:36
+//Types of variables
+let number = 123;
+let word = 'Alan Vargas';
+let float = 12.343;
+let bool = true;
+let bool2 = false;
+let double = 124932432432.3324;
+
+console.log(number + word + float + bool + bool2 + double);
