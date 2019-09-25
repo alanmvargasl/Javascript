7bfbcf8fd6f50fbd4a4a2331778b0b8a34ba6bff
alanmvargas <alanvargas@devgama.com>
2019-09-25 17:19:47 -0500

    natives correct version

---
+++ vstudio-js/natives.js
@@ -0,0 +1,40 @@
+/* let paragraph = `If the world froze over and everybody
+died and you and i were the only ones alive, id say i am sorry youve
+made it, its more than you deserve!`;
+
+
+for(i=0;i<paragraph.length;i++)
+{
+   console.log(paragraph.slice((i-1),i));
+    
+} */
+/* 
+let myString = new String(`Hello`);
+console.log(myString);
+console.log(typeof myString); */
+
+/* List of all Natives
+String()
+Number()
+Boolean()
+Object()
+Function()
+Symbol()
+
+*/
+
+/* Primitive Versions
+Array()
+RegExp()
+*/
+
+/* 
+Date()
+Error() 
+*/
+
+/* let myPrimitive = "This is my primitive";
+console.log(typeof myPrimitive);
+myPrimitive = myPrimitive.toLowerCase();
+console.log(typeof myPrimitive); */
+
