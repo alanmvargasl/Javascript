488b35c0fbb24651f708777b2165ba30d43f3a74
alanmvargas <alanvargas@devgama.com>
2019-09-25 17:00:53 -0500

    Constructors

---
+++ vstudio-js/constructors.js
@@ -0,0 +1,15 @@
+/* let car ={
+    brand: "Mercedes Benz",
+    year: 2019,
+    model: "Premium"
+};
+ */
+
+ function Car(a,b,c)
+ {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+ }
+ let myCar = new Car("A",2,"C");
+ console.log(myCar);
