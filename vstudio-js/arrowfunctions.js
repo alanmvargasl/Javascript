cf44493dc0bf48de7f462f14d40bba3dc1a4b2f2
alanmvargas <alanvargas@devgama.com>
2019-09-27 18:33:00 -0500

    ArrowFunctions

---
+++ vstudio-js/arrowfunctions.js
@@ -0,0 +1,8 @@
+let myFunction = () => {console.log("This is the functions message");}
+myFunction();
+
+let myFunction2 = (name) => {console.log(`My Name is ${name}`);}
+myFunction2("Alan");
+
+let name = [ "alan", "Chris", "verenice"];
+name.map((name) => { console.log(`Hello ${name}`)} );
