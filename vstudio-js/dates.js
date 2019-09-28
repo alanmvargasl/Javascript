64a03bbbc20816ac1f7ae7aa1a8684a4ad90b598
alanmvargas <alanvargas@devgama.com>
2019-09-27 19:14:54 -0500

    Dates Sintaxis

---
+++ vstudio-js/dates.js
@@ -0,0 +1,6 @@
+let myBirthdate = new Date(`September 27, 2019 07:01:23`);
+console.log(myBirthdate);
+let myBirthdate2 = new Date(`2019-09-27T07:01:23`);
+console.log(myBirthdate2);
+let myBirthdate3 = new Date(2019,08,27);
+console.log(myBirthdate3);
