28670a9142304fe7c2315fcfff4a91a5057ac18c
alanmvargas <alanvargas@devgama.com>
2019-09-29 15:18:50 -0500

    Array Methods

---
+++ vstudio-js/Array-methods.js
@@ -0,0 +1,22 @@
+let names = [`Alan` , `Chris`];
+let others = [`Vere`, `Sophie`];
+
+let numbers = [4,8,15,16,23,42];
+let fibonacci = [1,1,2,3,5,8,13,21,34,55];
+
+/* var combine = numbers.concat(fibonacci); //Combines both arrays.
+console.log(combine); */
+
+/* console.log(fibonacci.join(`.`)); //it joins them with the value given!
+console.log(numbers.shift()); // Removes the first data of an array
+console.log(numbers);
+
+numbers.unshift(1,2,3); // Adds numbers or data at the beggining of an array
+console.log(numbers); */
+
+/* console.log(names); 
+console.log(names.reverse());//it reverses the names position. it switches them
+console.log(names.sort()); // puts all names or strings in alphabetical order!
+ */
+
+/*  console.log(others.indexOf(`Sphie`)); // finds the value in the array and if it exists it tells you the location where it is inside the array. */
