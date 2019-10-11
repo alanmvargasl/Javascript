0164854c4d0a53f1604ca4e0cc832cd72adb7dec
alanmvargas <alanvargas@devgama.com>
2019-10-11 12:39:49 -0500

    School Homeword

---
+++ Cetec - Binario y Octal Operaciones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
